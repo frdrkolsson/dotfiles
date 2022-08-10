6be99fcd23979a0b95e059fae0a1969b7c2117b6
Fredrik Olsson <fredrikolsson@MacBook-Pro-som-tillhor-Fredrik.local>
2022-08-10 10:54:46 +0200

    Setup direnv and fish

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -x EDITOR nvim
+    set -x DIRENV_LOG_FORMAT
+
+    # kubernetes cluster environment
+    set -x AWS_PROFILE standout-kubernetes
+    set -x AWS_ACCESS_KEY_ID $(aws configure get aws_access_key_id)
+    set -x AWS_SECRET_ACCESS_KEY $(aws configure get aws_secret_access_key)
+
+    set -x AWS_S3_REGION eu-north-1
+end
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+direnv hook fish   | source
+direnv export fish | source
+
+# prompt
+starship init fish | source
