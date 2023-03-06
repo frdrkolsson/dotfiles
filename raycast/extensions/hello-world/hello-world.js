4f4051026066bba6cefeb26f916389a328ea21f7
Fredrik Olsson <fredrikolsson@MacBook-Pro-som-tillhor-Fredrik-2.local>
2023-03-06 15:41:01 +0100

    Add raycast

---
+++ raycast/extensions/hello-world/hello-world.js
@@ -0,0 +1,38 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/hello-world.tsx
+var hello_world_exports = {};
+__export(hello_world_exports, {
+  default: () => Command
+});
+module.exports = __toCommonJS(hello_world_exports);
+var import_api = require("@raycast/api");
+var import_jsx_runtime = require("react/jsx-runtime");
+function Command() {
+  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_api.Form, {
+    children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_api.Form.TextInput, {
+      title: "City",
+      id: "city"
+    })
+  });
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vLi4vLi4vLi4vQ29kZS9yYXljYXN0L2hlbGxvLXdvcmxkL3NyYy9oZWxsby13b3JsZC50c3giXSwKICAic291cmNlc0NvbnRlbnQiOiBbImltcG9ydCB7IEZvcm0gfSBmcm9tIFwiQHJheWNhc3QvYXBpXCI7XG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIENvbW1hbmQoKSB7XG4gIHJldHVybiAoXG4gICAgPEZvcm0+XG4gICAgICA8Rm9ybS5UZXh0SW5wdXRcbiAgICAgICB0aXRsZT0nQ2l0eSdcbiAgICAgICBpZD0nY2l0eScgLz5cbiAgICA8L0Zvcm0+XG4gIClcbn1cbiJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBLGlCQUFxQjtBQUFyQjtBQUVlLFNBQVIsVUFBMkI7QUFDaEMsU0FDRSw0Q0FBQztBQUFBLElBQ0Msc0RBQUMsZ0JBQUssV0FBTDtBQUFBLE1BQ0EsT0FBTTtBQUFBLE1BQ04sSUFBRztBQUFBLEtBQU87QUFBQSxHQUNiO0FBRUo7IiwKICAibmFtZXMiOiBbXQp9Cg==
