007c790ab97595f668075f5e29db7412317cc688
Fredrik Olsson <fredrik.olsson@precisit.com>
2023-04-03 09:20:14 +0200

    fish: Update config and abbrevations
    Add options to abbrevated commands and use main instead of master for git abbrevations

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,10 @@ if status is-interactive
     set -x DIRENV_LOG_FORMAT
 
     # kubernetes cluster environment
-    set -x AWS_PROFILE standout-kubernetes
+    set -x AWS_PROFILE precisit
     set -x AWS_ACCESS_KEY_ID $(aws configure get aws_access_key_id)
     set -x AWS_SECRET_ACCESS_KEY $(aws configure get aws_secret_access_key)
 
-    set -x AWS_S3_REGION eu-north-1
     set -x RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@1.1)"
 end
 
