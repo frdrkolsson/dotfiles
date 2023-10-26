486d7324ecc2da854c1eca1c643b39b3845cca4b
Fredrik Olsson <51903586+frdrkolsson@users.noreply.github.com>
2023-10-26 11:22:44 +0200

    chezmoi: Add tpm and asdf plugins on apply

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -18,3 +18,10 @@ chsh -s $(which fish)
 echo 'Increasee key repeat rate...'
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+echo 'Install tmux package manager...'
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo 'Add ruby and nodejs to asdf...'
+asdf plugin add ruby
+asdf plugin add nodejs
