86cd568a898f5d6d6c4235eb19e14fbe556a610c
Fredrik Olsson <fredrik.f.olsson@ehalsomyndigheten.se>
2026-03-14 16:53:03 +0100

    nvim: use template for blink
    bonus fixes

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@ local opts = { noremap = true, silent = true }
 
 keymap.set("n", "<Leader>w", "<CMD>:w!<CR>", opts)
 keymap.set("n", "<Leader><Leader>", "<c-^>", opts)
+keymap.set("n", "<Leader>r", vim.lsp.buf.format, { noremap = true, silent = true, desc = "Format file" })
 keymap.set("n", "<Leader>s", ":%s//g<Left><Left>", { noremap = true, silent = false }) -- Search and Replace
 keymap.set("n", "<C-j>", vim.diagnostic.goto_next, opts)
 keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, opts)
