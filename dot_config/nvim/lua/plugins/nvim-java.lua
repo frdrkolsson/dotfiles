f93c42fa07991f71ef46c66cc9a4f5b6fe19d702
Fredrik Olsson <fredrik.f.olsson@ehalsomyndigheten.se>
2026-04-08 09:07:02 +0200

    ehm: update ehm config

--- dot_config/nvim/lua/plugins/nvim-java.lua
+++ dot_config/nvim/lua/plugins/nvim-java.lua
@@ -1,7 +1,29 @@
 return {
   'nvim-java/nvim-java',
   config = function()
-    require('java').setup()
+    require('java').setup({
+      jdk = {
+        auto_install = false
+      },
+      spring_boot_tools = {
+        enable = false
+      }
+    })
+    vim.lsp.config('jdtls', {
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-21",
+                path = "/home/fredrik/.local/share/mise/installs/java/temurin-21.0.10+7.0.LTS/",
+                default = true,
+              }
+            }
+          }
+        }
+      }
+    })
     vim.lsp.enable('jdtls')
   end,
 }
